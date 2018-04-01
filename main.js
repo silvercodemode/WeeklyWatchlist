4943ef11d5a5bf15c97b3c992c5a82ccf26e1167
MP7373 <martinpayne73@gmail.com>
2018-04-01 03:57:04 -0400

    fixed case statement in convertNumericWeekdayToString function have the week start with Sunday not Monday

--- main.js
+++ main.js
@@ -436,12 +436,12 @@ function convertNumericMonthToString(numericMonth) {
 
 function convertNumericWeekdayToString(numericWeekday) {
     switch (numericWeekday) {
-        case 0: return 'Monday'
-        case 1: return 'Tuesday'
-        case 2: return 'Wednesday'
-        case 3: return 'Thursday'
-        case 4: return 'Friday'
-        case 5: return 'Saturday'
-        case 6: return 'Sunday'
+        case 0: return 'Sunday'
+        case 1: return 'Monday'
+        case 2: return 'Tuesday'
+        case 3: return 'Wednesday'
+        case 4: return 'Thursday'
+        case 5: return 'Friday'
+        case 6: return 'Saturday'
     }
 }
