6225815226f289a558e7514884488fe0bbc1ed76
MP7373 <martinpayne73@gmail.com>
2018-05-04 16:34:42 -0400

    updated serviceworker.js to newer verson of google airhorn sample code to try and get it to function correctly

--- serviceworker.js
+++ serviceworker.js
@@ -1,20 +1,29 @@
+const cacheName = 'weeklyWatchlist'
+
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open('airhorner').then(cache => {
+    e.waitUntil(caches.open(cacheName).then(cache => {
+        const timeStamp = Date.now()
         return cache.addAll([
             '/',
-            '/index.html',
-            '/styles.css',
-            '/main.js',
-            '/assets/list-icon-256.png'
-        ])
+            `/index.html?timestamp=${timeStamp}`,
+            `/style.css?timestamp=${timeStamp}`,
+            `/main.js?timestamp=${timeStamp}`,
+            `/assets/list-icon-256.png?timestamp=${timeStamp}`
+        ]).then(() => self.skipWaiting())
     }))
 })
 
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim())
+})
+
+//(async () => {
 self.addEventListener('fetch', e => {
     console.log(e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request)
+    caches.open(cacheName)
+        .then(cache => cache.match(e.request, {ignoreSearch: true}))
+        .then(response => {
+            return response || fetch(e.request)
         })
-    )
 })
+//})()
