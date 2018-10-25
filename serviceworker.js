88fc911a86bc6e699c544d2530a7cd169914676b
MP7373 <martinpayne73@gmail.com>
2018-10-24 22:00:41 -0400

    ran prettier on main.js and serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,26 +1,27 @@
-const cacheName = 'weeklyWatchlist'
-const baseUrl = '/WeeklyWatchlist/'
+const cacheName = "weeklyWatchlist";
+const baseUrl = "/WeeklyWatchlist/";
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName)
-        .then(cache => {
-            console.log('Cache opened')
-            return cache.addAll([
-                baseUrl,
-                `${baseUrl}index.html`,
-                `${baseUrl}style.css`,
-                `${baseUrl}main.js`
-            ])
-        })
-        .catch(error => console.log(error))
-    )
-})
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("Cache opened");
+        return cache.addAll([
+          baseUrl,
+          `${baseUrl}index.html`,
+          `${baseUrl}style.css`,
+          `${baseUrl}main.js`
+        ]);
+      })
+      .catch(error => console.log(error))
+  );
+});
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request)
-            .then(response => {
-                return response || fetch(e.request)
-            })
-    )
-})
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
